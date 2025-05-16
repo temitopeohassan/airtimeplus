4d799ec2da9fe7ff55aef7b87c8bc1c0f3f2b54c
temitopeohassan <temitope.hassan.2@gmail.com>
2025-05-16 12:33:59 +0100

    files reset

--- frontend/app/providers.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { ReactNode } from 'react';
-import { CartProvider } from '@/hooks/use-cart';
-
-export function Providers({ children }: { children: ReactNode }) {
-  return <CartProvider>{children}</CartProvider>;
-}
